3b959bc497caa518ffded853ff3fafca491e7722
mktimes5 <mktimes5@gmail.com>
2016-09-01 13:11:46 -0700

    initalized basic button on editor

--- static/plugin.js
+++ static/plugin.js
@@ -1,26 +1,3 @@
-/*
-CKEDITOR.plugins.add( 'sing2ocls',
-{
-	//requires: 'widget',
-
-	//icons: 'icons/twocols.png',
-
-	init: function( editor ) {
-			editor.widgets.add( 'sing2cols'),
-				//code here
-		} );
-	}
-
-	// create a button
-	editor.widgets.add( 'sing2cols' ), {
-		button: 'Singular 2 columns'
-	} );
-
-
-} );
-*/
-
-
 CKEDITOR.plugins.add( 'singtwocols',
 {
 	init: function( editor )
@@ -30,7 +7,7 @@ CKEDITOR.plugins.add( 'singtwocols',
 				exec : function( editor )
 			{    
 				
-				editor.insertHtml( '<p>sing2cols</p>' , 'unfiltered_html' );
+				editor.insertHtml( '<p> singtwocols </p>' , 'unfiltered_html' );
 			}
 		});
 
@@ -38,10 +15,11 @@ CKEDITOR.plugins.add( 'singtwocols',
 		
 		editor.ui.addButton( 'singtwocols',
 			{
-				label: 'singular 2 cols',
+				label: 'Insert singtwo phrase',
 				command: 'singtwocolscom',
 				icon: this.path + 'icons/twocols.png'
 			} );
 
 	}
 } );
+
